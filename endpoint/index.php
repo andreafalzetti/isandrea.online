71819541ad611599978ed513aa4bc1fd62d82702
andreafalzetti <afalzetti@gmail.com>
2016-04-13 23:06:22 +0100

    isandrea.online update

--- endpoint/index.php
+++ endpoint/index.php
@@ -10,10 +10,10 @@
     }
 
     // Fetch timestamp
-    $last_updated = file_get_contents('../private/last_updated.txt');
+    $last_updated = file_get_contents('../../private/last_updated.txt');
 
     // Last update within 10 minutes?
-    $status = (strtotime($last_updated) > strtotime("-10 minutes") ? 'online' : 'offline');
+    $status = (strtotime($last_updated) > strtotime("-2 minutes") ? 'online' : 'offline');
 ?>
 
 <!DOCTYPE html>
@@ -31,9 +31,9 @@
 
         <title>
             <?php if($status == 'online'):?>
-                Yeah &#128077;, Kingsley is online!
+                Yeah &#128077;, Andrea is online!
             <?php else: ?>
-                No &#128078;, Kingsley is offline!
+                No &#128078;, Andrea is offline!
             <?php endif; ?>
         </title>
 
@@ -44,17 +44,17 @@
         <?php endif;?>
 
 
-        <meta content="Is Kingsley Online?" property="og:title" />
+        <meta content="Is Andrea Online?" property="og:title" />
         <meta content="website" property="og:type" />
-        <meta content="Don't know if Kingsley is online? Find out here" property="og:description" />
-        <meta content="http://iskingsley.online" property="og:url" />
+        <meta content="Don't know if Andrea is online? Find out here" property="og:description" />
+        <meta content="http://isandrea.online" property="og:url" />
         <meta content="/preview-image.png" property="og:image" />
 
         <meta content="summary_large_image" name="twitter:card" />
-        <meta content="Is Kingsley Online?" name="twitter:title" />
-        <meta content="Don't know if Kingsley is online? Find out here" name="twitter:description" />
+        <meta content="Is Andrea Online?" name="twitter:title" />
+        <meta content="Don't know if Andrea is online? Find out here" name="twitter:description" />
         <meta content="/preview-image.png" name="twitter:image" />
-        <meta content="http://iskingsley.online" name="twitter:url" />
+        <meta content="http://isandrea.online" name="twitter:url" />
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
@@ -68,10 +68,10 @@
             html,body, .status {
                 height: 100%;
             }
-            body.kingsley--is-online{
+            body.andrea--is-online{
                 background-color:#dff0d8;
             }
-            body.kingsley--is-offline{
+            body.andrea--is-offline{
                 background-color: #f2dede;
             }
             .status{
@@ -125,16 +125,17 @@
         </style>
 
         <script>
-            !function(I,s,O,n,l,y,N){I.GoogleAnalyticsObject=O;I[O]||(I[O]=function(){
-            (I[O].q=I[O].q||[]).push(arguments)});I[O].l=+new Date;y=s.createElement(n);
-            N=s.getElementsByTagName(n)[0];y.src=l;N.parentNode.insertBefore(y,N)}
-            (window,document,'ga','script','//www.google-analytics.com/analytics.js');
-
-            ga('create', 'UA-21583989-3', 'auto');
-            ga('send', 'pageview');
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        
+          ga('create', 'UA-76361463-1', 'auto');
+          ga('send', 'pageview');
+        
         </script>
     </head>
-    <body id="app" class="kingsley--is-<?php echo $status;?>">
+    <body id="app" class="andrea--is-<?php echo $status;?>">
         <main role="main" class="status">
             <?php if($status == 'online'){
                 echo '<i class="fa fa-thumbs-up" aria-hidden="true"></i>';
@@ -144,7 +145,7 @@
         </main>
 
         <div class="fork-on-github">
-            <a href="https://github.com/kingsloi/iskingsley.online" target="_blank"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff;"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>
+            <a href="https://github.com/andreafalzetti/isandrea.online" target="_blank"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff;"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>
         </div>
     </body>
 </html>
