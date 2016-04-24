a36c4c050240c0cff5c0a257b7c696dd7d97ad04
andreafalzetti <afalzetti@gmail.com>
2016-04-24 16:07:03 +0100

    removed config.php

--- endpoint/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  
-  unset($config);
-  $config = new stdClass();
-  $config->password_file = '../../private/isandrea.online.passwd';
-  $config->updated_file = '../../private/last_updated.txt';
-  $config->timespan = '-2 minutes';
-  $config->timezone = "Europe/London";
-  $config->clickDesk = "all_ag9zfmNsaWNrZGVza2NoYXRyDwsSBXVzZXJzGPnTzuETDA";
